f78b16a729cb00d85a8ffd4e78cadf05e3314eca
Dilitha Peiris <dilitha@hsenidoutsourcing.com>
2022-12-19 13:35:38 +0900

    new commit

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
-  static const marvelPublicKey = "8b9d5b85bc5310ad95ce115a1d197e46";
-  static const marvelPrivateKey = "fbd165187c039e6a16cc1a398aa2c8e8b6f39d7d";
+  //Create keys and add it here
+  static const marvelPublicKey = "abcd";
+  static const marvelPrivateKey = "1234";
 }
